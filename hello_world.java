10323574bee8d95917c861454d856a7c0ea78c62
vinaywebdev <vinuvinay9505@gmail.com>
2024-02-10 19:51:35 +0530

    This is Hello world java commit

---
+++ hello_world.java
@@ -0,0 +1,8 @@
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I am learning java");
+        System.out.println("It is awesome");
+
+    }
+}
