e9726e80233dcba81152bb171b56e3845c729d49
vinaywebdev <vinuvinay9505@gmail.com>
2024-02-12 18:22:23 +0530

    This is a commit about operators in java

---
+++ operatorspractice.java
@@ -0,0 +1,62 @@
+public class operatorspractice { 
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 6;
+                  //Arithematic operators
+        /* System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b); */
+                 //Unary operators incremental
+        /* System.out.println(a);     //20
+        System.out.println(++a);   //21
+        System.out.println(a++);   //21
+        System.out.println(a);     //22  */
+                //Unary operators decremental
+        /* System.out.println(a);     //20
+        System.out.println(--a);   //19
+        System.out.println(a--);   //19
+        System.out.println(a);     //18  */
+               //Relational operators
+        
+         /* System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b); */
+
+                //Conditional operators &&,||.
+         /* System.out.println(a > b && b<a);  // T&T=T
+         System.out.println(a>b && a<b);    // T&F=F
+         System.out.println(a<b && b<a);    // F&T=F
+         System.out.println(a<b && b>a);    // F&F=F  */
+
+          /*System.out.println(a>b || b<a);  //T||T=T
+         System.out.println(a>b || a<b);    //T||F=T
+         System.out.println(a<b || b<a);    //F||T=T
+         System.out.println(a<b || b>a);    //F||F=F  
+         System.out.println(a>b||b<a) This is also applicable with out spaces*/
+
+                  //Assignment operators
+
+              /*  a = a+5;
+             System.out.println(a); Question ? */
+
+             /* a += 5;
+             System.out.println(a);
+             a -= 5;
+             System.out.println(a);
+             a *= 5;
+             System.out.println(a);
+             a /= 5;            
+             System.out.println(a);
+             a %= 5;
+             System.out.println(a); */
+
+
+        
+    }
+    
+}
